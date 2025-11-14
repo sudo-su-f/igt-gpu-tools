sudo-su-f/igt-gpu-tools

commit 480fa21b82c2c711537d27cc73a6ebca9eb4f1ac

lib: Add NVL-S support
Add NVL-S device info.

v2: Fix indentation/spacing for PantherLake and Novalake in
lib/intel_chipset.h (Kamil)

Signed-off-by: Himanshu Girotra <himanshu.girotra@intel.com>
Signed-off-by: Shekhar Chauhan <shekhar.chauhan@intel.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

lib/intel_chipset.h
@@ -93,6 +93,7 @@ struct intel_device_info {
 	bool is_lunarlake : 1;
 	bool is_battlemage : 1;
 	bool is_pantherlake : 1;
+	bool is_novalake: 1;
 
 	const struct intel_cmds_info *cmds_info;
 	const char *codename;
@@ -209,7 +210,8 @@ void intel_check_pch(void);
 #define IS_PONTEVECCHIO(devid)	(intel_get_device_info(devid)->is_pontevecchio)
 #define IS_LUNARLAKE(devid)	(intel_get_device_info(devid)->is_lunarlake)
 #define IS_BATTLEMAGE(devid)	(intel_get_device_info(devid)->is_battlemage)
-#define IS_PANTHERLAKE(devid)  (intel_get_device_info(devid)->is_pantherlake)
+#define IS_PANTHERLAKE(devid)	(intel_get_device_info(devid)->is_pantherlake)
+#define IS_NOVALAKE(devid)	(intel_get_device_info(devid)->is_novalake)
 
 #define IS_GEN(devid, x)	(intel_get_device_info(devid)->graphics_ver == x)
 

lib/intel_device_info.c
@@ -539,6 +539,17 @@ static const struct intel_device_info intel_pantherlake_info = {
 	.cmds_info = &xe2_cmds_info,
 };
 
+static const struct intel_device_info intel_novalake_info = {
+	.graphics_ver = 30,
+	.graphics_rel = 04,
+	.display_ver = 35,
+	.has_4tile = true,
+	.has_oam = true,
+	.is_novalake = true,
+	.codename = "novalake",
+	.cmds_info = &xe2_cmds_info,
+};
+
 #define INTEL_PCI_ID_INIT(_id, _info) { \
 	.vendor_id = 0x8086, .device_id = (_id), \
 	.subvendor_id = PCI_MATCH_ANY, .subdevice_id = PCI_MATCH_ANY, \
@@ -656,6 +667,8 @@ static const struct pci_id_match intel_device_match[] = {
 	INTEL_BMG_IDS(INTEL_PCI_ID_INIT, &intel_battlemage_info),
 
 	INTEL_PTL_IDS(INTEL_PCI_ID_INIT, &intel_pantherlake_info),
+
+	INTEL_NVLS_IDS(INTEL_PCI_ID_INIT, &intel_novalake_info),
 
 	INTEL_PCI_ID_INIT(PCI_MATCH_ANY, &intel_generic_info),
 };

commit 69d0aa3606a205aec90d6145bb20cbae755e8559

lib: Replace NVL with NVL-S
Fix NVL-S definition, by replacing Novalake with Novalake-S.

Signed-off-by: Shekhar Chauhan <shekhar.chauhan@intel.com>
Fixes: 480fa21b82c2 ("lib: Add NVL-S support")
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
Reviewed-by: Ashutosh Dixit <ashutosh.dixit@intel.com>

lib/intel_chipset.h
@@ -93,7 +93,7 @@ struct intel_device_info {
 	bool is_lunarlake : 1;
 	bool is_battlemage : 1;
 	bool is_pantherlake : 1;
-	bool is_novalake: 1;
+	bool is_novalake_s: 1;
 	bool is_crescentisland: 1;
 
 	const struct intel_cmds_info *cmds_info;
@@ -212,7 +212,7 @@ void intel_check_pch(void);
 #define IS_LUNARLAKE(devid)	(intel_get_device_info(devid)->is_lunarlake)
 #define IS_BATTLEMAGE(devid)	(intel_get_device_info(devid)->is_battlemage)
 #define IS_PANTHERLAKE(devid)	(intel_get_device_info(devid)->is_pantherlake)
-#define IS_NOVALAKE(devid)	(intel_get_device_info(devid)->is_novalake)
+#define IS_NOVALAKE_S(devid)	(intel_get_device_info(devid)->is_novalake_s)
 #define IS_CRESCENTISLAND(devid)	(intel_get_device_info(devid)->is_crescentisland)
 
 #define IS_GEN(devid, x)	(intel_get_device_info(devid)->graphics_ver == x)

lib/intel_device_info.c
@@ -539,14 +539,14 @@ static const struct intel_device_info intel_pantherlake_info = {
 	.cmds_info = &xe2_cmds_info,
 };
 
-static const struct intel_device_info intel_novalake_info = {
+static const struct intel_device_info intel_novalake_s_info = {
 	.graphics_ver = 30,
 	.graphics_rel = 04,
 	.display_ver = 35,
 	.has_4tile = true,
 	.has_oam = true,
-	.is_novalake = true,
-	.codename = "novalake",
+	.is_novalake_s = true,
+	.codename = "novalake_s",
 	.cmds_info = &xe2_cmds_info,
 };
 
@@ -678,7 +678,7 @@ static const struct pci_id_match intel_device_match[] = {
 
 	INTEL_PTL_IDS(INTEL_PCI_ID_INIT, &intel_pantherlake_info),
 
-	INTEL_NVLS_IDS(INTEL_PCI_ID_INIT, &intel_novalake_info),
+	INTEL_NVLS_IDS(INTEL_PCI_ID_INIT, &intel_novalake_s_info),
 
 	INTEL_CRI_IDS(INTEL_PCI_ID_INIT, &intel_crescentisland_info),
 